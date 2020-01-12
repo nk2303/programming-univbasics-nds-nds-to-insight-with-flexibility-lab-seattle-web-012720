f6d2c7d28e5fcd4fcd3ca0118a8f7c34379038d7
Ngan Kim Khong <ngankhong03@gmail.com>
2020-01-12 07:35:57 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -85,7 +85,7 @@ def movies_with_directors_set(source)
   # becomes... [[{:title => "Test", :director_name => "A"}], ...[], ... []]
   array = []
   source.length.times do |hash|
-    source[hash][:movie]
+    array.push(source[hash][:movie])
   return array
 end
 
